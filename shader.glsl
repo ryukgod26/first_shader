301acd026c9b5e66c60186241179f3b4f92039e0
Eklavya <126102004+ryukgod26@users.noreply.github.com>
2025-09-21 14:48:39 +0530

    Update shader.glsl

--- shader.glsl
+++ shader.glsl
@@ -1,10 +1,26 @@
+vec3 palette( in float t )
+{
+    vec3 a = vec3(0.520, 0.608, 0.608);
+    vec3 b = vec3(0.219, 0.478, 0.480);
+    vec3 c = vec3(0.750, 0.539, -1.929);
+    vec3 d = vec3(-1.372, -1.038, -0.705);
+
+
+    return a + b*cos( 6.283185*(c*t+d) );
+}
+
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     // Normalized pixel coordinates (from 0 to 1)
     vec2 uv = fragCoord/iResolution.xy * 2.0 -1.0;
     uv.x *= iResolution.x / iResolution.y;
+     vec2 uv0 = uv;
+   vec3 finalCol = vec3(0.0);
+    for(float i = 0.0; i< 3.0; i++){
     
-    float d  = length(uv);
+    
+     uv = fract(uv * 1.5) - 0.5;
+    float d  = length(uv) * exp(-length(uv0));
     
     d = sin(d*8. + iTime )/8.;
   //  d -= 0.5;
@@ -12,6 +28,11 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     
    //d = step(0.1,d);
   //d = smoothstep(0.0,0.1,d);
-  d = 0.01 /d;
-    fragColor = vec4(d,d,d,1.0);
+ // d = 0.01 /d;
+    d = pow(0.01/d,1.2);
+  
+    vec3 col = palette(length(uv0) + iTime);
+    finalCol += col *d;
+    }
+    fragColor = vec4(finalCol,1.0);
 }
